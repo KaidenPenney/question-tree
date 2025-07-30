65cc8600c46e96ae6c948a258cce47a9c8725ede
Kaiden Penney <mrpenney1030@gmail.com>
2025-07-29 17:58:54 -0700

    forgot to save

--- SpringBoot QuestionTree/src/main/java/com/questiontree/api/questiontreecode/WebConfig.java
+++ SpringBoot QuestionTree/src/main/java/com/questiontree/api/questiontreecode/WebConfig.java
@@ -7,13 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
     @Bean
-    public WebMvcConfigurer corsConfiguerer(){
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://127.0.0.1:5500")
-                    .allowedMethods("*");
+                    .allowedOrigins("https://javbittin.com")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*");
             }
         };
     }
